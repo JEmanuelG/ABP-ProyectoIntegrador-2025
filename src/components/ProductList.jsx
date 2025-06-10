9cd6aae9134156f7bfffdcd96ccee4e7ea3d3346
Emanuel Guaráz <guarazjemanuel@gmail.com>
2025-06-10 12:42:08 -0300

    Actualizo components

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,6 @@
 function Card({title, price}) {
     return(
-        <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font ">
             <div className="container px-5 py-24 mx-auto">
               <div className="flex flex-wrap -m-4">
                 <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
